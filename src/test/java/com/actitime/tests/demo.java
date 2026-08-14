ca1a3f8a37dc2722ffb654f8de650be55572f347
ambarishpanda <aapanda45@gmail.com>
2026-08-14 17:17:23 +0530

    new testcases added

---
+++ src/test/java/com/actitime/tests/demo.java
@@ -0,0 +1,12 @@
+package com.actitime.tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo {
+
+	@Test
+	public void git() {
+		Reporter.log("this is a new test class",true);
+	}
+}
