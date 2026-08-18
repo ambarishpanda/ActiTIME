0f63cdd648fd5febbcd03ce0e4d172ec03acce6d
ambarishpanda <aapanda45@gmail.com>
2026-08-18 19:24:07 +0530

    remove try catch block

--- src/main/java/com/actitime/pages/EditTypeOfWorkPage.java
+++ src/main/java/com/actitime/pages/EditTypeOfWorkPage.java
@@ -26,22 +26,16 @@ public class EditTypeOfWorkPage extends BasePage {
 	private WebElement saveButton;
 	
 	public void editName() throws AWTException, IOException, InterruptedException {
-		try {
-			wait.until(ExpectedConditions.visibilityOf(editWorkName));
-			Robot robo = new Robot();
-			robo.keyPress(KeyEvent.VK_CONTROL);
-			robo.keyPress(KeyEvent.VK_A);
-			robo.keyRelease(KeyEvent.VK_CONTROL);
-			robo.keyRelease(KeyEvent.VK_A);
-			robo.keyPress(KeyEvent.VK_BACK_SPACE);
-			robo.keyRelease(KeyEvent.VK_BACK_SPACE);
-			Thread.sleep(3000);
-			editWorkName.sendKeys(ExcelLibrary.readData("work", 1, 1));
-		} catch (Exception e) {
-			wait.until(ExpectedConditions.visibilityOf(editWorkName));
-			editWorkName.clear();
-			editWorkName.sendKeys(ExcelLibrary.readData("work", 1, 1));
-		}
+		wait.until(ExpectedConditions.visibilityOf(editWorkName));
+		Robot robo = new Robot();
+		robo.keyPress(KeyEvent.VK_CONTROL);
+		robo.keyPress(KeyEvent.VK_A);
+		robo.keyRelease(KeyEvent.VK_CONTROL);
+		robo.keyRelease(KeyEvent.VK_A);
+//		robo.keyPress(KeyEvent.VK_BACK_SPACE);
+//		robo.keyRelease(KeyEvent.VK_BACK_SPACE);
+		Thread.sleep(3000);
+		editWorkName.sendKeys(ExcelLibrary.readData("work", 1, 1));
 	}
 	
 	public void clickOnSaveButton() throws InterruptedException {
